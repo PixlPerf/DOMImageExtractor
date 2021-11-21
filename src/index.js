e0c5dcc08b24634f9865864ff1f14d66660d32c2
Ash Boy <85341173+theashboy@users.noreply.github.com>
2021-11-22 02:57:50 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,5 +8,14 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
-const str =
+
+const parser = new DOMParser();
+
+const htmlString =
   '<div style="font-family:-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif; font-size:14px;"><div><br></div><p data-identifyelement="801">Hi there,</p><div data-identifyelement="802"><br data-identifyelement="803"></div><p data-identifyelement="804">I need a refund for the headphones that I purchased last week. My order ID is #53467. The product was damaged when I received it. Can you please tell me how I can get a refund?</p><div data-identifyelement="805"><span dir="ltr" style="color: rgb(18, 52, 77); font-family: Arial; font-size: 15px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; white-space: pre-wrap; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">88112</span></div><p data-identifyelement="807">Best,<img src="https://attachment.freshdesk.com/inline/attachment?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NzMwMDAyNTQ0NjAsImRvbWFpbiI6Im5ld2FjY291bnQxNjM1MzMzNjAzMTI0LmZyZXNoZGVzay5jb20iLCJhY2NvdW50X2lkIjoyMTA1NTI2fQ.7o4FPLpvSyRmpCkqCUw9b2RCmnYtmyQZSUUWWVGuT8M" style="width: auto;" class="fr-fil fr-dib" data-attachment="[object Object]" data-id="73000254460">Matt.</p></div>';
+
+const parsedDoc = parser.parseFromString(htmlString, "text/html");
+
+var imgList = parsedDoc.querySelectorAll("img");
+
+console.log("ImageList", imgList);
